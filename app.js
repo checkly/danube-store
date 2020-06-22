1861b37fc05b832b45f51bfc49640ba82ee1f7d9
Giovanni Rago <ragog90@gmail.com>
2020-06-22 15:32:57 +0200

    Change routing, add books endpoint

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const path = require('path')
+const fs = require('fs')
 const express = require('express')
-const history = require('connect-history-api-fallback')
+var cors = require('cors')
+// const history = require('connect-history-api-fallback')
 
 const app = express()
 const port = process.env.PORT || 5000
 
-app.use(history())
+// app.use(history())
 
 app.use(express.json())
+app.use(cors())
 app.use(express.static(__dirname + '/vue/dist'))
 app.get(/.*/), (req,res) => res.sendFile(__dirname + '/vue/dist/index.html')
+
+app.get('/books', (req, res) => {
+    const rawData = fs.readFileSync('books.json')
+    const books = JSON.parse(rawData)
+    console.log(books)
+    res.status('200').json(books)
+})
 
 app.get('*', (req, res) => {
     res.status('404').json({
